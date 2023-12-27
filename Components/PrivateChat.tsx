cd92dfb6999b398f6e6bc1de3bd86be24ce01f03
akshatpant3002 <akshatpant3002@gmail.com>
2023-12-27 18:04:50 -0500

    try again

--- Components/PrivateChat.tsx
+++ Components/PrivateChat.tsx
@@ -18,28 +18,83 @@ function PrivateChat(props: PrivateChatProps): JSX.Element {
   const [privateComments, setPrivateComments] = useState<Comment[]>([]);
   const [newCommentContent, setNewCommentContent] = useState("");
 
+  const formatDate = (createdAt: string): string => {
+    const now = new Date();
+    const createdDate = new Date(createdAt); // Parse the string into a Date object
+    const diffMs = now.getTime() - createdDate.getTime(); // difference in milliseconds
+    const diffMins = Math.round(diffMs / 60000); // minutes
+    const diffHrs = Math.round(diffMins / 60); // hours
+    const diffDays = Math.round(diffHrs / 24); // days
+  
+    if (diffMins < 60) {
+      return `${diffMins} minutes ago`;
+    } else if (diffHrs < 24) {
+      return `${diffHrs} hours ago`;
+    } else if (diffDays < 7) {
+      return `${diffDays} days ago`;
+    } else {
+      // Format the date to show in "MM/DD/YYYY" format
+      return createdDate.toLocaleDateString();
+    }
+  };
+
+  // <Text style={styles.userName}>{`${comment.profile.firstName} ${comment.profile.lastName}  ${formatDate(comment.)}`}</Text>
+
 
   useEffect(() => {
     fetchPrivateChat()
   }, []);
 
-  const renderComment = (comment: Comment) => {
-    console.log("HHBB state.user: ", state._id)
-    console.log("HHBB comment.authorID: ", comment.authorID)
-  
-    const isAuthor = String(comment.authorID).trim() === String(state._id).trim();
-    console.log("HHBB: ", comment)
-    return (
-      <View key={comment._id} style={styles.commentContainer}>
-          {!isAuthor && (
-              <Text style={styles.userName}>{`${comment.profile.firstName} ${comment.profile.lastName}`}</Text>
-          )}
-          <View style={isAuthor ? styles.authorSelf : styles.authorOther}>
-              <Text style={isAuthor ? {color: 'white'}: {color: 'black'}}>{comment.content}</Text>
-          </View>
-      </View>
-  );
+// Define the initial state with appropriate types and default values
+let lastAuthorId = '';
+let lastCommentDate = new Date(0);
+
+const renderComment = (comment: Comment, index: number) => {
+  const isAuthor = String(comment.authorID).trim() === String(state._id).trim();
+  let showDate = false;
+
+  if (lastAuthorId !== comment.authorID) {
+      showDate = true;
+  } else {
+      const currentCommentDate = new Date(comment.date);
+      if (currentCommentDate.getTime() - lastCommentDate.getTime() > 600000) {
+          showDate = true;
+      }
+  }
+
+  lastAuthorId = comment.authorID;
+  lastCommentDate = new Date(comment.date);
+
+  return (
+    <View key={comment._id} style={styles.commentContainer}>
+        <View style={styles.nameAndDateContainer}>
+            {!isAuthor && (
+                <>
+                    <Text style={styles.userName}>
+                        {`${comment.profile.firstName} ${comment.profile.lastName}`}
+                    </Text>
+                    {showDate && (
+                        <Text style={styles.dateText}>
+                            {formatDate(comment.date)}
+                        </Text>
+                    )}
+                </>
+            )}
+            {isAuthor && showDate && (
+                <Text style={styles.authorSelfDate}>
+                    {formatDate(comment.date)}
+                </Text>
+            )}
+        </View>
+        <View style={isAuthor ? styles.authorSelf : styles.authorOther}>
+            <Text style={isAuthor ? { color: 'white' } : { color: 'black' }}>
+                {comment.content}
+            </Text>
+        </View>
+    </View>
+);
 };
+
 
 async function postComment() {
   console.log("COMMENT POSTED")
@@ -112,6 +167,11 @@ const styles = StyleSheet.create({
 commentContainer: {
     marginVertical: 1,
 },
+dateText: {
+  color: 'gray',
+  marginLeft: 8
+  // Add any other styling you need for the date text
+},
 authorSelf: {
     textAlign: 'right',
     backgroundColor: colors.purple,
@@ -120,6 +180,8 @@ authorSelf: {
     margin: 5,
     padding: 10,
     borderRadius: 10,
+    flexDirection: 'row-reverse', 
+    
 },
 authorOther: {
     textAlign: 'left',
@@ -131,7 +193,8 @@ authorOther: {
     borderRadius: 10,
 },
 userName: {
-    color: 'grey',
+    color: colors.purple,
+    fontWeight: 500,
     alignSelf: 'flex-start',
     marginBottom: 0,
     marginLeft: 5
@@ -152,5 +215,14 @@ userName: {
   messageContainer: {
       flex: 1,
       marginTop: 10
-  }
+  },
+  nameAndDateContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start', // Align items to the start
+    alignItems: 'center',
+},
+authorSelfDate: {
+  color: 'gray',
+  marginLeft: 'auto', // Pushes the date to the far left
+},
 });
