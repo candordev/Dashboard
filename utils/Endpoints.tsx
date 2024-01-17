a1a75bb53ca64a524211c39d2d4a764ad0cdc54b
akshatpant3002 <akshatpant3002@gmail.com>
2024-01-17 15:45:04 -0500

    send button for update post

--- utils/Endpoints.tsx
+++ utils/Endpoints.tsx
@@ -7,14 +7,14 @@
 // }
 
 // local
- export const BASE_URL : string = `http://localhost:4000/api`;
+ //export const BASE_URL : string = `http://localhost:4000/api`;
 //export const BASE_URL : string = `http://${HOST}:4000/api`;
 
 // DEVELOPMENT
 // export const BASE_URL : string = `http://3.221.91.129:4000/api`;
 
 // PRODUCTION
-//export const BASE_URL: string = `https://candoradmin.com/api`;
+export const BASE_URL: string = `https://candoradmin.com/api`;
 
 export const Endpoints = {
   base: BASE_URL,
