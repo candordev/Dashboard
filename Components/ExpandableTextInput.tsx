d468712aaf1d3f18e082f5e770e1235ca09ac010
Srikar Parsi <srikar.parsi@expensify.com>
2024-04-11 20:21:54 -0400

    some more stuff

--- Components/ExpandableTextInput.tsx
+++ Components/ExpandableTextInput.tsx
@@ -8,26 +8,25 @@ import styles from "../Styles/styles";
 interface ExpandableTextInputProps {
   onInputChange: (text: string) => void;
   onSubmit: () => void;
+  style: any;
 }
 
 function ExpandableTextInput(props: ExpandableTextInputProps): JSX.Element {
   const [height, setHeight] = useState(40);
 
-
   const updateFirstInputHeight = debounce((height) => {
+    console.log(height);
     setHeight(height);
   }, 1); // Adjust the debounce time as needed
 
-
-
-
   return (
     <TextInput
-      style={[styles.textInput, { height: Math.max(40, height) }]}
+      style={[styles.textInput, { height: Math.max(40, height) }, props.style]}
       placeholder="Add a comment..."
       placeholderTextColor={colors.gray}
       multiline={true}
       onContentSizeChange={(event) => {
+        console.log("CONTENT SIZE CHANGED")
         updateFirstInputHeight(event.nativeEvent.contentSize.height);
       }}
       onChangeText={props.onInputChange}
--- Screens/ChatComponent.tsx
+++ Screens/ChatComponent.tsx
@@ -15,6 +15,7 @@ import { customFetch } from "../utils/utils";
 import Icon from "react-native-vector-icons/FontAwesome";
 import styles from "../Styles/styles";
 import Button from "../Components/Button";
+import ExpandableTextInput from "../Components/ExpandableTextInput";
 
 interface ChatComponentProps {
   phoneNumber: string; // Assuming phoneNumber is a string
@@ -195,45 +196,52 @@ const ChatComponent: React.FC<ChatComponentProps> = ({ phoneNumber }) => {
     }
   }, [page, phoneNumber]); // Runs after `page` is updated, and also accounts for phoneNumber changes
 
-  const renderMessage = ({ item }: { item: MessageItem }) => (
-    <View
-      style={{
-        alignSelf: item.author === "Leader" ? "flex-end" : "flex-start",
-        marginBottom: 10,
-      }}
-    >
-      <Text
-        style={{
-          color: colors.purple,
-          fontSize: 14, // Slightly larger font size
-          fontFamily: "Montserrat", // Set the fontFamily to Montserrat
-          alignSelf: item.author === "Leader" ? "flex-end" : "flex-start",
-          fontWeight: "500",
-        }}
-      >
-        {item.author}
-      </Text>
+  const renderMessage = ({ item }: { item: MessageItem }) => {
+    const rightMessage = item.author === "Leader" || item.author === "AI";
+
+    return (
       <View
         style={{
-          backgroundColor:
-            item.author === "Leader" ? colors.purple : colors.lightergray,
-          borderRadius: 20,
-          padding: 10,
-          marginTop: 2,
+          alignSelf: rightMessage ? "flex-end" : "flex-start",
+          marginBottom: 10,
+          marginRight: rightMessage ? 0 : 100,
+          marginLeft: rightMessage ? 100 : 0,
         }}
       >
         <Text
           style={{
-            color: item.author == "Leader" ? colors.white : colors.black,
+            color: colors.purple,
+            fontSize: 14, // Slightly larger font size
+            fontFamily: "Montserrat", // Set the fontFamily to Montserrat
+            alignSelf: rightMessage ? "flex-end" : "flex-start",
             fontWeight: "500",
-            fontFamily: "Montserrat",
+            marginHorizontal: 5,
           }}
         >
-          {item.content}
+          {item.author}
         </Text>
+        <View
+          style={{
+            backgroundColor: rightMessage ? colors.purple : colors.lightergray,
+            borderRadius: 20,
+            padding: 10,
+            paddingHorizontal: 12.5,
+            marginTop: 3,
+          }}
+        >
+          <Text
+            style={{
+              color: rightMessage ? colors.white : colors.black,
+              fontWeight: "500",
+              fontFamily: "Montserrat",
+            }}
+          >
+            {item.content}
+          </Text>
+        </View>
       </View>
-    </View>
-  );
+    );
+  };
 
   const handleOnEndReached = () => {
     console.log("End reached - should load more");
@@ -310,34 +318,30 @@ const ChatComponent: React.FC<ChatComponentProps> = ({ phoneNumber }) => {
       />
 
       <View
-        style={{
-          flexDirection: "row",
-          padding: 3,
-          alignItems: "center",
-          borderRadius: 20,
-          borderColor: colors.lightgray,
-          borderWidth: 1,
-        }}
+        style={[
+          styles.textInput,
+          {
+            flexDirection: "row",
+            alignItems: "center",
+            borderRadius: 20,
+            borderColor: colors.lightergray,
+            borderWidth: 1,
+          },
+        ]}
       >
-        <TextInput
+        <ExpandableTextInput
+          style={{ flex: 1, borderWidth: 0, }}
+          onInputChange={setInputText}
+          onSubmit={handleSendChat}
+        />
+        {/* <TextInput
           multiline
-          style={[
-            styles.textInput,
-            {
-              borderColor: "transparent",
-              flex: 1,
-              paddingHorizontal: 4,
-              paddingVertical: 4,
-              marginTop: 10,
-              marginLeft: 5,
-            },
-          ]}
+          style={{ flex: 1, outlineStyle: "none" as any, padding: 10 }}
           placeholder="Send a text..."
           placeholderTextColor={colors.gray}
           onChangeText={setInputText}
-          selectionColor={colors.lightgray} // Set to a color that fits your design
           value={inputText}
-        />
+        /> */}
         <TouchableOpacity
           onPress={handleSendChat}
           disabled={!inputText.trim()} // Disable the button when the input is empty
@@ -345,7 +349,7 @@ const ChatComponent: React.FC<ChatComponentProps> = ({ phoneNumber }) => {
           <Icon
             name="paper-plane"
             size={20}
-            style={{ margin: 5 }}
+            style={{ marginRight: 5 }}
             color={inputText.trim() ? colors.purple : colors.lightgray}
           />
         </TouchableOpacity>
