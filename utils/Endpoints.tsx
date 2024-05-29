cecffa58558c0a1da1bfa10a6243b083299235c8
Nishant Nagururu <nishantnvn05@gmail.com>
2024-05-29 12:02:12 -0400

    prod url

--- utils/Endpoints.tsx
+++ utils/Endpoints.tsx
@@ -8,7 +8,7 @@
 
 // local
 
-export const BASE_URL : string = `http://localhost:4000/api`;
+// export const BASE_URL : string = `http://localhost:4000/api`;
 
 // export const BASE_URL : string = `http://${HOST}:4000/api`;
 
@@ -16,7 +16,7 @@ export const BASE_URL : string = `http://localhost:4000/api`;
 //export const BASE_URL : string = `http://3.221.91.129:4000/api`;
 
 // PRODUCTION
-// export const BASE_URL: string = `https://candoradmin.com/api`;
+export const BASE_URL: string = `https://candoradmin.com/api`;
 
 
 //export const BASE_URL: string = `http://184.72.74.25:4000/api`;
